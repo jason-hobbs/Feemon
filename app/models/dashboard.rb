b4d21131391e4170c7bf2064e7356253184d3c88
grimmstede <grimmstede@feemon.com>
2014-05-26 20:40:38 -0500

    added references to dashboard

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,4 +1,8 @@
 class Dashboard < ActiveRecord::Base
   belongs_to :user
+  belongs_to :feed
+  belongs_to :entry
   has_many :users, dependent: :destroy
+  has_many :feeds
+  has_many :entries
 end
