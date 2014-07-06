4901a63c3ac33cbaac733f807e12063638897d77
grimmstede <grimmstede@feemon.com>
2014-07-06 18:36:32 -0500

    added new user instructions in notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ def create
   		@user = User.new(user_params)
   		if @user.save
 		    session[:user_id] = @user.id
-    		redirect_to @user, notice: "Thanks for signing up!"
+    		redirect_to @user, notice: "Thanks for signing up! Click edit to add sites to start tracking! Then click dashboard on the top navigation bar!  You can also go to Gravatar.com to upload your avatar pic."
   		else
     		render :new
   		end
