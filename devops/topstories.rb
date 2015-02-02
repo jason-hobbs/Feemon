a1638b846ca1903122f1b0feaca851cc716c27a3
Jason Hobbs <grimmstede@feemon.com>
2015-02-02 14:13:45 -0600

    topstories change

--- devops/topstories.rb
+++ devops/topstories.rb
@@ -13,7 +13,7 @@ conn.prepare("insert_entry", "insert into topstories (entry_id) values ($1)")
 conn.exec("delete from topstories")
 conn.exec( "SELECT title,id,url,updated_at FROM feeds" ) do |result|
   result.each do |url|
-    if url["title"] == 'Engadget' || url["title"] == 'Arstechnica' || url["title"] == 'U.S. News' || url["title"] == 'Polygon' || url["title"] == 'Fox News' || url["title"] == 'Joystiq' || url["title"] == 'The Verge' || url["title"] == 'Gizmodo' || url["title"] == 'Lifehacker'
+    if url["title"] == 'Engadget' || url["title"] == 'Arstechnica' || url["title"] == 'U.S. News' || url["title"] == 'Polygon' || url["title"] == 'Fox News' || url["title"] == 'The Verge' || url["title"] == 'Gizmodo' || url["title"] == 'Lifehacker'  || url["title"] == 'Yahoo News' || url["title"] == 'SBNation'
 
       feedid = url["id"]
       conn.exec( "SELECT id FROM entries where feed_id = #{feedid} order by id desc limit 1" ) do |entry|
