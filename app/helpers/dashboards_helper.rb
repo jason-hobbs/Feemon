40af4af26e73efe3e508171f36d2df259b27d1dd
Jason Hobbs <grimmstede@feemon.com>
2015-07-07 11:42:31 -0500

    added caching to topstories on dashboard

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,8 @@
 module DashboardsHelper
+
+  def cache_key_for_topstories
+    count          = Topstories.count
+    max_updated_at = Topstories.maximum(:updated_at).try(:utc).try(:to_s, :number)
+  end
+
 end
